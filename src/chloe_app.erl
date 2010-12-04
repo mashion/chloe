35b6b545ccf2386b87e235e57ed97fab9a68d37f
Trotter Cashion <cashion@gmail.com>
2010-12-04 18:11:59 -0500

    Getting started w/ rebar

---
+++ src/chloe_app.erl
@@ -0,0 +1,16 @@
+-module(chloe_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    chloe_sup:start_link().
+
+stop(_State) ->
+    ok.
