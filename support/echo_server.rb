9c530fa675041bb1d8297cc990163a45284c2a82
Trotter Cashion <cashion@gmail.com>
2011-03-25 10:14:20 -0400

    Refactor echo_server example to work on any host

--- support/echo_server.rb
+++ support/echo_server.rb
@@ -4,15 +4,25 @@ require 'net/http'
 require 'uri'
 
 set :public, File.dirname(__FILE__) + '/public'
+set :views,  File.dirname(__FILE__) + '/views'
 
 get '/' do
-  File.read(File.dirname(__FILE__) + '/public/index.html')
+  erb :index
+end
+
+get '/demo.js' do
+  content_type :js
+  erb :demo
 end
 
 post '/updates' do
   data = request.body.read
-  Net::HTTP.post_form(URI.parse("http://localhost:8901/send"),
+  Net::HTTP.post_form(URI.parse("http://#{server_name}:8901/send"),
                       {"data" => "Handled by Sinatra: #{data}"})
   puts "I got some data: #{data}"
   "success"
+end
+
+def server_name
+  @request.env["SERVER_NAME"]
 end
